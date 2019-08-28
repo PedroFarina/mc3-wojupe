444d1ff7395ec2f97ae7889447589b6f406494b4
Pedro Farina <pedrogfarina@gmail.com>
2019-08-28 17:55:13 -0300

    Adicionando xibs comuns

---
+++ Ping/Ping/View/TableViewCell/SwitchTableViewCell.swift
@@ -0,0 +1,32 @@
+//
+//  SwitchTableViewCell.swift
+//  Ping
+//
+//  Created by Pedro Giuliano Farina on 28/08/19.
+//  Copyright © 2019 Pedro Giuliano Farina. All rights reserved.
+//
+
+import UIKit
+
+class SwitchTableViewCell: UITableViewCell {
+    static let xib: String = "SwitchTableViewCell"
+    static let identifier: String = "SwitchTableViewCell"
+
+    @IBOutlet var lblTitle: UILabel!
+    @IBOutlet var onOff: UISwitch!
+    public var onOffChanged: ((UISwitch) -> Void)?
+
+    @IBAction func onOffOccur (_  sender: UISwitch) {
+        guard let onOffChanged = onOffChanged else {
+            return
+        }
+        onOffChanged(sender)
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
