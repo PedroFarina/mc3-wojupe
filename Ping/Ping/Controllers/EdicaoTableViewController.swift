a0ffb50d6792f1ad081d62782a8397d2a2a9f136
Julia Santos <juliasantos@10-44-48-211.0200.mack.br.iosda.org>
2019-08-30 15:07:55 -0300

    configurando stb edicao

---
+++ Ping/Ping/Controllers/EdicaoTableViewController.swift
@@ -0,0 +1,13 @@
+//
+//  EdicaoTableViewController.swift
+//  Ping
+//
+//  Created by Julia Santos on 30/08/19.
+//  Copyright © 2019 Pedro Giuliano Farina. All rights reserved.
+//
+
+import UIKit
+
+public class EdicaoTableViewController: UITableViewController {
+    
+}
